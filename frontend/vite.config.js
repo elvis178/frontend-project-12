7fb9d2489e2c36f745941faf137499fb0c0e1f7e
elvis178 <elvisss178@gmail.com>
2025-05-15 23:54:51 +0300

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,16 +5,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5002,
     proxy: {
+      // Проксируем запросы к API
       '/api': {
-        target: 'http://127.0.0.1:5001',
-        changeOrigin: true,
-        secure: false,
+        target: 'http://localhost:5001',
+      },
+      // Проксируем WebSocket соединения
+      '/socket.io': {
+        target: 'ws://localhost:5001',
+        ws: true,
+        rewriteWsOrigin: true,
       },
     },
   },
-  build: {
-    outDir: './dist',
-    emptyOutDir: true, // Очищаем папку перед сборкой
-  }
-})
+});
