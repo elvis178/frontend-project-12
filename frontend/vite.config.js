63d9a05c28df6bf85a534149e22e11aadcbd06f5
elvis178 <elvisss178@gmail.com>
2025-05-22 22:22:15 +0300

    returned the settings back

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,34 +1,29 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Критически важно для корректных путей
   server: {
     port: 5002,
     proxy: {
-
+      // Проксируем запросы к API
       '/api': {
-        target: 'http://127.0.0.1:5001',
-        changeOrigin: true,
+        target: 'http://localhost:5001',
+        changeOrigin: true
       },
-
+      // Проксируем WebSocket соединения
       '/socket.io': {
-        target: 'ws://127.0.0.1:5001',
+        target: 'ws://localhost:5001',
         ws: true,
-        changeOrigin: true,
         rewriteWsOrigin: true,
       },
     },
   },
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          formik: ['formik'],
-          reactBootstrap: ['react-bootstrap'],
-          reactBootstrapIcons: ['react-bootstrap-icons'],
-        },
-      },
-    },
-  },
+    outDir: './dist',
+    emptyOutDir: true,
+    sourcemap: true // Для отладки
+  }
 });
