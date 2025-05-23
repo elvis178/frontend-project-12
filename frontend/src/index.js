1c4b5005f74d72f3171499a27c02b9e954a4fdba
elvis178 <elvisss178@gmail.com>
2025-05-23 15:23:57 +0300

    fix index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import init from './init.jsx';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const app = async () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(await init());
+};
 
-App();
+app();
