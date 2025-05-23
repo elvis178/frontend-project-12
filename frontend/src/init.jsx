5c538ccd91aee77f8c43aaf63291b4f690cda3cc
elvis178 <elvisss178@gmail.com>
2025-05-23 15:43:48 +0300

    update frontend/src/init.jsx

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import App from './App';
-import 'react-toastify/dist/ReactToastify.css';
 
-const init = async () => {
-  return (
-    <React.StrictMode>
-      <App />
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-    </React.StrictMode>
-  );
-};
+const init = () => (
+  <React.StrictMode>
+    <App />
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+  </React.StrictMode>
+);
 
 export default init;
