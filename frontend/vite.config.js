bbbf4ee3d56f0a526a218de073e58c5c2acb5da3
elvis178 <elvisss178@gmail.com>
2025-05-23 20:16:29 +0300

    update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,39 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Критически важно для корректных путей
   server: {
     port: 5002,
     proxy: {
-      // Проксируем запросы к API
       '/api': {
         target: 'http://127.0.0.1:5001',
-        changeOrigin: true
+        changeOrigin: true,
       },
-      // Проксируем WebSocket соединения
       '/socket.io': {
         target: 'ws://127.0.0.1:5001',
         ws: true,
-        rewriteWsOrigin: true,
         changeOrigin: true,
+        rewriteWsOrigin: true,
+      },
+    },
+    historyApiFallback: true, // это для dev-сервера
+  },
+  preview: {
+    host: true,
+    strictPort: true,
+    historyApiFallback: true // Для SPA на Render.com
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          formik: ['formik'],
+          reactBootstrap: ['react-bootstrap'],
+          reactBootstrapIcons: ['react-bootstrap-icons'],
+        },
       },
     },
   },
-  build: {
-    outDir: './dist',
-    emptyOutDir: true,
-    sourcemap: true, // Для отладки
-    rollupOptions: {
-      input: './index.html'
-    }
-  },
-  preview: {
-    port: process.env.PORT || 10000, // Важно для Render
-    host: true,
-    strictPort: true,
-    historyApiFallback: true // Для SPA
-  }
 });
