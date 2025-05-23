3143d2770c3adcdede2542829bdaeec21507d44d
elvis178 <elvisss178@gmail.com>
2025-05-23 13:21:58 +0300

    fix vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,12 +10,12 @@ export default defineConfig({
     proxy: {
       // Проксируем запросы к API
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'http://127.0.0.1:5001',
         changeOrigin: true
       },
       // Проксируем WebSocket соединения
       '/socket.io': {
-        target: 'ws://localhost:5001',
+        target: 'ws://127.0.0.1:5001',
         ws: true,
         rewriteWsOrigin: true,
         changeOrigin: true,
