b6003d13565eff5df93ae435d27c58c0af943416
elvis178 <elvisss178@gmail.com>
2025-05-23 15:43:27 +0300

    update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,6 @@
 import ReactDOM from 'react-dom/client';
-import init from './init.jsx';
+import init from './init'; 
 import './index.css';
 
-const app = async () => {
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(await init());
-};
-
-app();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(init());
