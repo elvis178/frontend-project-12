e4c4fbe10b6db8b92198a2979205e5e45c46f5cf
elvis178 <elvisss178@gmail.com>
2025-05-23 22:52:28 +0300

    update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Критически важно для корректных путей
+  base: '/', // Важно для корректных путей
   server: {
     port: 5002,
     proxy: {
