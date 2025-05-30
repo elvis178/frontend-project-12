ea1e7ee18df148ef288b52ab634ee16476143455
elvis178 <elvisss178@gmail.com>
2025-05-30 23:45:21 +0300

    update init.jsx

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,23 +1,94 @@
-import React from 'react';
+import { StrictMode } from 'react';
+import i18next from 'i18next';
+import { Provider } from 'react-redux';
+import { initReactI18next, I18nextProvider } from 'react-i18next';
 import { ToastContainer } from 'react-toastify';
-import App from './App';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import * as filter from 'leo-profanity';
+import { io } from 'socket.io-client';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+import { apiMessages } from './api/apiMessages.js';
+import activeChannelReducer from './slices/activeChannelSlice.js';
+import modalsReducer from './slices/modalsSlice.js';
+import App from './App.jsx';
+import resources from './locales/index.js';
+import { apiChannels } from './api/apiChannels.js';
 
-const init = () => (
-  <React.StrictMode>
-    <App />
-    <ToastContainer
-      position="top-right"
-      autoClose={5000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="light"
-    />
-  </React.StrictMode>
-);
+const init = async () => {
+  const i18n = i18next.createInstance();
+
+  await i18n
+    .use(initReactI18next)
+    .init({
+      resources,
+      fallbackLng: 'ru',
+      interpolation: {
+        escapeValue: false,
+      },
+    });
+
+  const rootReducer = combineReducers({
+    [apiChannels.reducerPath]: apiChannels.reducer,
+    [apiMessages.reducerPath]: apiMessages.reducer,
+    activeChannel: activeChannelReducer,
+    modals: modalsReducer,
+  });
+
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+      .concat(apiChannels.middleware, apiMessages.middleware),
+  });
+
+  const socket = io();
+
+  socket.on('newChannel', (payload) => {
+    store.dispatch(apiChannels.util.updateQueryData('getChannels', undefined, (draft) => {
+      draft.push(payload);
+    }));
+  });
+
+  socket.on('removeChannel', (payload) => {
+    store.dispatch(apiChannels.util.updateQueryData('getChannels', undefined, (draft) => {
+      draft.filter((channel) => channel.id !== payload);
+    }));
+  });
+
+  socket.on('renameChannel', (payload) => {
+    store.dispatch(apiChannels.util.updateQueryData('getChannels', undefined, (draft) => {
+      const channel = draft.find((c) => c.id === payload.id);
+      channel.name = payload.name;
+    }));
+  });
+
+  socket.on('newMessage', (payload) => {
+    store.dispatch(apiMessages.util.updateQueryData('getMessages', undefined, (draft) => {
+      draft.push(payload);
+    }));
+  });
+
+  const rollbarConfig = {
+    accessToken: import.meta.env.ACCESS_TOKEN,
+    environment: 'production',
+  };
+
+  filter.add(filter.getDictionary('en'));
+  filter.add(filter.getDictionary('ru'));
+
+  return (
+    <RollbarProvider config={rollbarConfig}>
+      <I18nextProvider i18n={i18n}>
+        <ErrorBoundary>
+          <Provider store={store}>
+            <StrictMode>
+              <App />
+              <ToastContainer />
+            </StrictMode>
+          </Provider>
+        </ErrorBoundary>
+      </I18nextProvider>
+    </RollbarProvider>
+  );
+};
 
 export default init;
