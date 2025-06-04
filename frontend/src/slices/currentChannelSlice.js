6dddd2dca37f5e09caa80d85282ec8909687268a
elvis178 <elvisss178@gmail.com>
2025-06-04 21:59:36 +0300

    update currentChannelSlice.js

---
+++ frontend/src/slices/currentChannelSlice.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+
+export const defaultChannel = {
+  id: '1',
+  name: 'general',
+  removable: false,
+};
+
+const initialState = {
+  currentChannel: defaultChannel,
+};
+
+const activeChannelSlice = createSlice({
+  name: 'activeChannel',
+  initialState,
+  reducers: {
+    selectActiveTab: (state, { payload }) => {
+      state.currentChannel = payload;
+    },
+  },
+});
+
+export const activeChannelSelector = (state) => state.activeChannel.currentChannel;
+export const { selectActiveTab } = activeChannelSlice.actions;
+export default activeChannelSlice.reducer;
