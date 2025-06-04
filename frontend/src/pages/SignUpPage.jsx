721942f8be40046122f0c3179f178666608195ec
elvis178 <elvisss178@gmail.com>
2025-06-04 21:57:00 +0300

    update SignUpPage.jsx

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,156 +1,122 @@
-import {
-  useState,
-  useRef,
-  useEffect,
-} from 'react';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { Form, Button, Card } from 'react-bootstrap';
-//import * as yup from 'yup';
-import axios from 'axios';
 import { useTranslation } from 'react-i18next';
+import axios from 'axios';
+import { useRef, useState } from 'react';
+import {
+  Container, Row, Col, Card, Form, Button,
+} from 'react-bootstrap';
 import { useFormik } from 'formik';
-import useAuth from '../hooks/index.jsx';
+import { useNavigate } from 'react-router-dom';
+import avatarSignUp from '../assets/avatar-signUp.jpg';
 import routes from '../routes.js';
-import signUpImage from '../assets/avatar-signUp.jpg';
 import createLoginValidator from '../utils.js';
+import useAuth from '../hooks/index.jsx';
 
-const SignUp = () => {
+const SignUpPage = () => {
   const { t } = useTranslation();
-  const auth = useAuth();
-  const inputRef = useRef();
+  const inputEl = useRef(null);
+  const [authFailed, setAuthFailed] = useState(false);
   const navigate = useNavigate();
-
-  const [signUpError, setSignUpError] = useState(false);
-
-  useEffect(() => {
-    inputRef.current.focus();
-  }, []);
-
-  /*const validationSchema = yup.object().shape({
-    username: yup
-      .string()
-      .trim()
-      .required(t('yup.errors.required'))
-      .min(3, t('yup.errors.userNameLength'))
-      .max(20, t('yup.errors.userNameLength')),
-    password: yup
-      .string()
-      .trim()
-      .required(t('yup.errors.required'))
-      .min(6, t('yup.errors.passwordLength')),
-    retypePassword: yup
-      .string()
-      .trim()
-      .required(t('yup.errors.required'))
-      .oneOf([yup.ref('password'), null], t('yup.errors.passwordsDiffer')),
-  });*/
+  const auth = useAuth();
 
   const formik = useFormik({
     initialValues: {
       username: '',
       password: '',
-      retypePassword: '',
+      confirmPassword: '',
     },
-    createLoginValidator,
-    validateOnChange: false,
-    onSubmit: async (values) => {
-      setSignUpError(false);
-      const { username, password } = values;
-      axios.post(routes.apiSignupPath(), { username, password })
-        .then((response) => {
-          auth.logIn(response.data);
-          navigate('/');
-        })
-        .catch((error) => {
-          if (!error.isAxiosError) {
-            toast.error(t('errors.unknown'));
-            return;
-          }
-
-          if (error.response.status === 409) {
-            setSignUpError(true);
-            inputRef.current.select();
-          } else {
-            toast.error(t('yup.errors.networkError'));
-          }
-        });
+    validationSchema: createLoginValidator(t),
+    onSubmit: async ({ username, password }, { setSubmitting }) => {
+      try {
+        const { data } = await axios.post(routes.signUpPath(), { username, password });
+        auth.logIn(data.token, data.username);
+        navigate(routes.main());
+      } catch (err) {
+        setSubmitting(false);
+        if (err.response.status === 409) {
+          setAuthFailed(true);
+          inputEl.current.select();
+        }
+        throw err;
+      }
     },
   });
 
   return (
-    <div className="container-fluid h-100">
-      <div className="row justify-content-center align-content-center h-100">
-        <div className="col-12 col-md-8 col-xxl-6">
-          <Card>
-            <Card.Body className="row p-5">
-              <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
-                <img src={signUpImage} className="rounded-circle" alt={t('titles.signup')} />
+    <Container fluid className="h-100">
+      <Row className="justify-content-center align-content-center h-100">
+        <Col className="col-12 col-md-8 col-xxl-6">
+          <Card className="shadow-sm">
+            <Card.Body className="d-flex flex-column flex-md-row justify-content-around align-items-center p-5">
+              <div>
+                <img src={avatarSignUp} className="rounded-circle" alt={t('signUpForm.signUp')} />
               </div>
-              <Form onSubmit={formik.handleSubmit} className="col-12 col-md-6 mt-3 mt-mb-0">
-                <h1 className="text-center mb-4">{t('titles.signup')}</h1>
-                <Form.Group className="mb-3 form-floating">
-                  <Form.Control
-                    id="username"
-                    name="username"
-                    type="text"
-                    placeholder={t('forms.signup.userName')}
-                    onChange={formik.handleChange}
-                    value={formik.values.username}
-                    ref={inputRef}
-                    disabled={formik.isSubmitting}
-                    isInvalid={(formik.touched.username && formik.errors.username) || signUpError}
-                    noValidate
-                  />
-                  <Form.Label htmlFor="username">{t('forms.signup.userName')}</Form.Label>
-                  <Form.Text className="invalid-tooltip">
-                    {formik.errors.username}
-                  </Form.Text>
-                </Form.Group>
-                <Form.Group className="mb-3 form-floating">
-                  <Form.Control
-                    id="password"
-                    name="password"
-                    type="password"
-                    placeholder={t('forms.signup.password')}
-                    onChange={formik.handleChange}
-                    disabled={formik.isSubmitting}
-                    isInvalid={(formik.touched.password && formik.errors.password) || signUpError}
-                    noValidate
-                  />
-                  <Form.Label htmlFor="password">{t('forms.signup.password')}</Form.Label>
-                  <Form.Text className="invalid-tooltip">
-                    {formik.errors.password}
-                  </Form.Text>
-                </Form.Group>
-                <Form.Group className="mb-3 form-floating">
-                  <Form.Control
-                    id="retypePassword"
-                    name="retypePassword"
-                    type="password"
-                    placeholder={t('forms.signup.retypePassword')}
-                    onChange={formik.handleChange}
-                    disabled={formik.isSubmitting}
-                    isInvalid={(formik.touched.retypePassword && formik.errors.retypePassword)}
-                    noValidate
-                  />
-                  <Form.Label htmlFor="retypePassword">{t('forms.signup.retypePassword')}</Form.Label>
-                  <Form.Control.Feedback type="invalid" tooltip>
-                    {signUpError ? t('yup.errors.userAlreadyExists')
-                      : t(formik.errors.retypePassword)}
-                  </Form.Control.Feedback>
-                </Form.Group>
-                <Button variant="outline-primary" disabled={formik.isSubmitting} className="w-100 mb-3" type="submit">
-                  {t('buttonNames.signup')}
-                </Button>
+              <Form className="w-50" onSubmit={formik.handleSubmit}>
+                <h1 className="text-center mb-4">{t('signUpForm.signUp')}</h1>
+                <fieldset>
+                  <Form.Group className="form-floating mb-3" controlId="username">
+                    <Form.Control
+                      autoComplete="username"
+                      name="username"
+                      required
+                      placeholder={t('signUpForm.username')}
+                      onChange={formik.handleChange}
+                      value={formik.values.username}
+                      ref={inputEl}
+                      autoFocus
+                      isInvalid={(formik.touched.username
+                        && !!formik.errors.username) || authFailed}
+                      onBlur={formik.handleBlur}
+                    />
+                    <Form.Label>{t('signUpForm.username')}</Form.Label>
+                    <Form.Control.Feedback type="invalid" tooltip>{formik.errors.username}</Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group className="form-floating mb-3" controlId="password">
+                    <Form.Control
+                      type="password"
+                      autoComplete="new-password"
+                      aria-describedby="passwordHelpBlock"
+                      name="password"
+                      required
+                      placeholder={t('loginForm.password')}
+                      onChange={formik.handleChange}
+                      value={formik.values.password}
+                      ref={inputEl}
+                      isInvalid={(formik.touched.password
+                        && !!formik.errors.password) || authFailed}
+                      onBlur={formik.handleBlur}
+                    />
+                    <Form.Label>{t('loginForm.password')}</Form.Label>
+                    <Form.Control.Feedback type="invalid" tooltip>{formik.errors.password}</Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group className="form-floating mb-4" controlId="confirmPassword">
+                    <Form.Control
+                      type="password"
+                      autoComplete="new-password"
+                      aria-describedby="passwordHelpBlock"
+                      name="confirmPassword"
+                      required
+                      placeholder={t('signUpForm.confirmPassword')}
+                      onChange={formik.handleChange}
+                      value={formik.values.confirmPassword}
+                      ref={inputEl}
+                      isInvalid={(formik.touched.confirmPassword
+                      && !!formik.errors.confirmPassword) || authFailed}
+                      onBlur={formik.handleBlur}
+                    />
+                    <Form.Control.Feedback type="invalid" tooltip>
+                      {authFailed ? t('signUpForm.existsUser') : formik.errors.confirmPassword}
+                    </Form.Control.Feedback>
+                    <Form.Label>{t('signUpForm.confirmPassword')}</Form.Label>
+                  </Form.Group>
+                  <Button type="submit" variant="outline-primary" className="w-100">{t('signUpForm.signUpButton')}</Button>
+                </fieldset>
               </Form>
             </Card.Body>
           </Card>
-        </div>
-      </div>
-    </div>
-
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
-export default SignUp;
+export default SignUpPage;
