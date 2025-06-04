eef96f899031fb285fb99ef2c32253f976bc6812
elvis178 <elvisss178@gmail.com>
2025-06-04 21:58:49 +0300

    fix utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,17 +1,40 @@
 import * as Yup from 'yup';
+import { useTranslation } from 'react-i18next';
 
-const createLoginValidator = () => {
+export const useLanguage = () => {
+  const { i18n } = useTranslation();
+  
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
+  return { changeLanguage };
+};
+
+export const channelValidator = (t, channelsNames) => Yup.object().shape({
+  name: Yup.string()
+    .required(t('modal.schema.required'))
+    .min(3, t('modal.schema.minMax'))
+    .max(20, t('modal.schema.minMax'))
+    .notOneOf(channelsNames, t('modal.schema.notOneOf')),
+});
+
+const createLoginValidator = (t) => {
   return Yup.object().shape({
     username: Yup.string()
       .transform(value => value?.trim())
-      .min(3, 'errors.usernameTooShort')
-      .max(20, 'errors.usernameTooLong')
-      .required('errors.requiredField'),
+      .min(3, t('modal.schema.minMax'))
+      .max(20, t('modal.schema.minMax'))
+      .required(t('modal.schema.required')),
 
     password: Yup.string()
-      .min(6, 'errors.passwordTooShort')
-      .required('errors.requiredField')
-  })
-}
+      .min(6, t('signUpForm.minSymbolForPassword'))
+      .required(t('modal.schema.required')),
+      
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password'), null], t('signUpForm.passwordsMustMatch'))
+      .required(t('modal.schema.required'))
+  });
+};
 
 export default createLoginValidator;
