eb94f718414a0e2d25a1603a078dd5907c1199ea
elvis178 <elvisss178@gmail.com>
2025-06-04 21:53:08 +0300

    update Channels

--- frontend/src/components/Channels.jsx
+++ frontend/src/components/Channels.jsx
@@ -5,11 +5,11 @@ import { useTranslation } from 'react-i18next';
 import * as filter from 'leo-profanity';
 import { PlusSquare } from 'react-bootstrap-icons';
 import { useGetChannelsQuery } from '../api/apiChannels.js';
-import { selectActiveTab, activeChannelSelector, defaultChannel } from '../slices/activeChannelSlice.js';
+import { selectActiveTab, activeChannelSelector, defaultChannel } from '../slices/currentChannelSlice.js';
 import AddChannel from './modal/AddModal.jsx';
 import RemoveChannel from './modal/RemoveModal.jsx';
 import RenameChannel from './modal/RenameModal.jsx';
-import { closeModal, openModal } from '../slices/modalsSlice.js';
+import { closeModal, openModal } from '../slices/channelModalsSlice.js';
 
 const Channels = () => {
   const { t } = useTranslation();
@@ -26,40 +26,45 @@ const Channels = () => {
     dispatch(openModal({ type, channel }));
   };
 
-  const removableChannel = (channel) => (
-    <Dropdown role="group" className="d-flex btn-group">
-      <Button className="w-100 rounded-0 text-start text-truncate" variant={variant(channel)} onClick={() => dispatch(selectActiveTab(channel))}>
-        <span className="me-1"># </span>
-        {filter.clean(channel.name)}
-      </Button>
-      <Dropdown.Toggle className="flex-grow-0 dropdown-toggle-split" variant={variant(channel)}>
-        <span className="visually-hidden">{t('channels.setupChannel')}</span>
-      </Dropdown.Toggle>
-      <Dropdown.Menu>
-        <Dropdown.Item role="button" onClick={() => showModal('removing', channel)}>{t('channels.dropdownButtonRemove')}</Dropdown.Item>
-        <Dropdown.Item role="button" onClick={() => showModal('renaming', channel)}>{t('channels.dropdownButtonRename')}</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-  );
-
-  const notRemovableChannel = (channel) => !channel.removable && (
+  const renderChannelButton = (channel) => (
     <Button
-      type="button"
       className="w-100 rounded-0 text-start text-truncate"
       variant={variant(channel)}
       onClick={() => dispatch(selectActiveTab(channel))}
     >
       <span className="me-1"># </span>
-        {channel.name}
+      {channel.removable ? filter.clean(channel.name) : channel.name}
     </Button>
   );
 
+  const renderRemovableChannel = (channel) => (
+    <Dropdown role="group" className="d-flex btn-group">
+      {renderChannelButton(channel)}
+      <Dropdown.Toggle className="flex-grow-0 dropdown-toggle-split" variant={variant(channel)}>
+        <span className="visually-hidden">{t('channels.setupChannel')}</span>
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item 
+          role="button" 
+          onClick={() => showModal('removing', channel)}
+        >
+          {t('channels.dropdownButtonRemove')}
+        </Dropdown.Item>
+        <Dropdown.Item 
+          role="button" 
+          onClick={() => showModal('renaming', channel)}
+        >
+          {t('channels.dropdownButtonRename')}
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+
   useEffect(() => {
+    if (!channelsRef.current) return;
+    
     if (activeChannel.id === defaultChannel.id) {
-      channelsRef.current.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
+      channelsRef.current.scrollTo({ top: 0, behavior: 'smooth' });
     } else {
       channelsRef.current.scrollTop = channelsRef.current.scrollHeight;
     }
@@ -67,6 +72,12 @@ const Channels = () => {
 
   return (
     <>
+      {/* Модальные окна вынесены на верхний уровень */}
+      {modals.type === 'adding' && <AddChannel onHide={hideModal} />}
+      {modals.type === 'removing' && <RemoveChannel onHide={hideModal} />}
+      {modals.type === 'renaming' && <RenameChannel onHide={hideModal} />}
+
+      {/* Основной интерфейс каналов */}
       <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
         <b>{t('channels.title')}</b>
         <Button
@@ -74,12 +85,12 @@ const Channels = () => {
           className="text-primary"
           variant="link"
           onClick={() => showModal('adding', activeChannel)}
+          aria-label={t('channels.addChannel')}
         >
           <PlusSquare size={20} />
-          <span className="visually-hidden">+</span>
         </Button>
-        {modals.type === 'adding' && (<AddChannel onHide={hideModal} />)}
       </div>
+
       <Nav
         as="ul"
         id="channels-box"
@@ -88,11 +99,9 @@ const Channels = () => {
       >
         {channels.map((channel) => (
           <Nav.Item as="li" key={channel.id} className="w-100">
-            {channel.removable ? removableChannel(channel) : notRemovableChannel(channel)}
+            {channel.removable ? renderRemovableChannel(channel) : renderChannelButton(channel)}
           </Nav.Item>
         ))}
-        {modals.type === 'removing' && (<RemoveChannel onHide={hideModal} />)}
-        {modals.type === 'renaming' && (<RenameChannel onHide={hideModal} />)}
       </Nav>
     </>
   );
