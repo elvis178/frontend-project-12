e2fdc431237ad1ca749a2140bf893be58033e238
elvis178 <elvisss178@gmail.com>
2025-06-06 20:19:22 +0300

    linter

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,53 +1,30 @@
-import js from '@eslint/js';
 import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import react from 'eslint-plugin-react';
+import pluginReact from 'eslint-plugin-react';
 
+/** @type {import('eslint').Linter.Config[]} */
 export default [
+  { files: ['**/*.{js,jsx}'] },
+  { ignores: ['/node_modules/', 'dist/'] },
   {
-    ignores: ['dist'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: globals.browser,
+      sourceType: 'module',
+    },
   },
   {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      react,
-    },
     settings: {
       react: {
-        version: 'detect',
+        version: '18.3',
       },
     },
+  },
+  pluginReact.configs.flat.recommended,
+  {
     rules: {
-      // Точки с запятой — разрешены, но не требуются (можно и с `;`, и без)
-      'semi': 'off',                 // Выключаем стандартное правило
-      'no-extra-semi': 'off',        // Разрешаем лишние `;` (если вдруг появятся)
-
-      // Круглые скобки у стрелочных функций — опциональны (можно и с `()`, и без)
-      'arrow-parens': 'off',         // Выключаем требование скобок
-      
-      // Разрешаем краткую запись стрелочных функций (без `{}`, если тело в одну строку)
-      'arrow-body-style': ['warn', 'as-needed'],
-
-      // Остальные правила
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      semi: 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
     },
   },
 ];
