6f13c4b60b3a8550787dc956e1921a999baae56c
elvis178 <elvisss178@gmail.com>
2025-06-10 01:10:11 +0300

    fix index.jsx

---
+++ frontend/src/components/modal/index.jsx
@@ -0,0 +1,17 @@
+import AddChannel from './AddModal.jsx';
+import RemoveChannel from './RemoveModal.jsx';
+import RenameChannel from './RenameModal.jsx';
+
+const MODAL_COMPONENTS = {
+  adding: AddChannel,
+  removing: RemoveChannel,
+  renaming: RenameChannel,
+};
+
+const ChannelModal = ({ type, onHide }) => {
+  const ModalComponent = MODAL_COMPONENTS[type];
+
+  return ModalComponent ? <ModalComponent onHide={onHide} /> : null;
+};
+
+export default ChannelModal;
