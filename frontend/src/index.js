3cfa1420a55615ef7602066da21f58f527ea71b4
elvis178 <elvisss178@gmail.com>
2025-06-09 18:43:29 +0300

    update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client';
+import { io } from 'socket.io-client';
 import init from './init.jsx';
 import './index.css';
 
+const socket = io();
+
 const app = async () => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(await init());
+  root.render(await init(socket));
 };
 
 app();
