bf97dc656b6bfe81809a3e729c9611ae4da33253
elvis178 <elvisss178@gmail.com>
2025-06-10 12:05:14 +0300

    fix eslint.config.js

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,34 +1,29 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import stylistic from '@stylistic/eslint-plugin';
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
 
-export default [
+export default defineConfig([
   stylistic.configs.recommended,
-  pluginJs.configs.recommended,
   {
-    files: [
-      '**/*.{js,ts,tsx}',
-    ],
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
     languageOptions: {
-      globals: {
-        ...globals.node,
-
-        document: 'readonly',
-        localStorage: 'readonly',
-        process: 'readonly',
-      },
-      parserOptions: {
-        projectService: true,
-      },
-    },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-      '@stylistic/semi': ['error', 'always'],
-      '@stylistic/arrow-parens': ['error', 'always'],
+      globals: { ...globals.node, ...globals.browser },
     },
   },
   {
     ignores: ['dist/'],
   },
-];
+  {
+    rules: {
+      '@stylistic/semi': 'off',
+      '@stylistic/arrow-parens': 'off',
+      'no-console': 'warn',
+    },
+  },
+])
